2b709ec6f7ddeaf34b69269c335374409a3bbeda
conrado <capistrano.conrado@gmail.com>
2024-10-22 20:55:27 -0300

    pastas componentes

---
+++ projeto-Viagens--/src/componentes/carousel/index.tsx
@@ -0,0 +1,28 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './style.css'
+
+export default function Carousel() {
+  return (
+    <div className='container'>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
